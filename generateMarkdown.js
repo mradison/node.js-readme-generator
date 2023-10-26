9bc53df5d87a5c0ef4f5097176c2e1913db7389b
madison reynolds <madison.reynolds@phoenix.edu>
2023-10-25 23:20:15 -0700

    removed the comments to clean up the code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,32 +1,25 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license){
-    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)]`; 
-  } 
+  if (license) {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)]`;
+  }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license){
+  if (license) {
     return license
-  } else if (license === 'N/A'){
+  } else if (license === 'N/A') {
     return ''
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== 'N/A'){
+  if (license !== 'N/A') {
     return `## License`
-  }  else {
+  } else {
     return ''
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# Title of Project: ${data.name}
 
